13cb34d6f5079405ba8bbbc5d343c61bf1b69111
MrCiprian89 <mr.ciprian89@gmail.com>
2015-02-07 11:27:28 -0500

    Added buttons and their event listeners

--- SlotMachineStephenCiprian/Scripts/game.ts
+++ SlotMachineStephenCiprian/Scripts/game.ts
@@ -4,6 +4,12 @@ var stage: createjs.Stage;
 // Game Objects 
 var game: createjs.Container;
 var background: createjs.Bitmap;
+var spinButton: createjs.Bitmap;
+var maxBetButton: createjs.Bitmap;
+var betTenButton: createjs.Bitmap;
+var betOneButton: createjs.Bitmap;
+var powerButton: createjs.Bitmap;
+var resetButton: createjs.Bitmap;
 
 function init() {
     canvas = document.getElementById("canvas");
@@ -20,13 +26,61 @@ function gameLoop() {
 }
 
 // Event handlers
-
+function spinButtonClick() {
+    console.log("a button was clicked");
+}
+function maxBetButtonClick() {
+    console.log("a button was clicked");
+}
+function betOneButtonClick() {
+    console.log("a button was clicked");
+}
+function betTenButtonClick() {
+    console.log("a button was clicked");
+}
+function powerButtonClick() {
+    console.log("a button was clicked");
+}
+function resetButtonClick() {
+    console.log("a button was clicked");
+}
 
 //function to instantiate all game controls for UI
 function createUI() {
-    //instantiate my background
+    //create background
     background = new createjs.Bitmap("assets/images/background.png");
     game.addChild(background);
+    //create spin button
+    spinButton = new createjs.Bitmap("assets/images/spin_button.png");
+    game.addChild(spinButton);
+    spinButton.x = 308.7;
+    spinButton.y = 346.8;
+    spinButton.addEventListener("click", buttonclick);
+    //create max bet button;
+    maxBetButton = new createjs.Bitmap("assets/images/bet_max_button.png");
+    game.addChild(maxBetButton);
+    maxBetButton.x = 160.3;
+    maxBetButton.y = 350.5;
+    //create bet one button
+    betOneButton = new createjs.Bitmap("assets/images/bet_one_button.png");
+    game.addChild(betOneButton);
+    betOneButton.x = 118.5;
+    betOneButton.y = 338;
+    //create bet ten button
+    betTenButton = new createjs.Bitmap("assets/images/bet_ten_button.png");
+    game.addChild(betTenButton);
+    betTenButton.x = 118.4;
+    betTenButton.y = 373.8;
+    //create power button
+    powerButton = new createjs.Bitmap("assets/images/power_button.png");
+    game.addChild(powerButton);
+    powerButton.x = 36.5;
+    powerButton.y = 338.5;
+    //create reset button
+    resetButton = new createjs.Bitmap("assets/images/reset_button.png");
+    game.addChild(resetButton);
+    resetButton.x = 76.5;
+    resetButton.y = 338.5;
 }
 
 // Our Game Kicks off in here
@@ -35,6 +89,7 @@ function main() {
     game = new createjs.Container();
     //Create User interface
     createUI();
-    //Add the game container to the stage
+    //Add the game container to the stage1
     stage.addChild(game);
 }
+
